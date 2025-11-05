8e451b8ff9a2dea5c8452709cb2e689f6b137764
rodrigoges <rgestevao@gmail.com>
2025-11-05 16:04:32 -0300

    feat: starting the project

---
+++ src/main/java/br/com/glic/GlicApplication.java
@@ -0,0 +1,13 @@
+package br.com.glic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlicApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GlicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/glic/GlicApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.glic;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlicApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
